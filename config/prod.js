37bf00c6bceaf2f52f0a65ed2ebfd841467f2c19
continuestudying <continuestudying@gmail.com>
2018-10-12 19:57:48 +0700

    Setup Environment for Product

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod.js
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID, // 955824015646-8t7gsvrl5673o50de43nq49hs3uhgaa8.apps.googleusercontent.com
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,   // 7CqIBshq8iG3EdkE7bK6rYt_
+    mongoURI: process.env.MONGO_URI,    // mongodb://username:1234qwer@ds129233.mlab.com:29233/survey-production
+    cookieKey: process.env.COOKIE_KEY
+}
