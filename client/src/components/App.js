a533547f60a45cee36f015807c51bce7cb712258
continuestudying <continuestudying@gmail.com>
2018-10-13 14:12:24 +0700

    Setup Redux

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Hi There</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
