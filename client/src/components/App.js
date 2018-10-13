9e9906cacf4eb17d4591431a134ca77941b5e162
continuestudying <continuestudying@gmail.com>
2018-10-13 14:31:10 +0700

    Setup React Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import axios from 'axios';
+
+const Header = () => <h1>Header</h1>;
+const DashBoard = () => <h1>DashBoard</h1>;
+const SurveyNew = () => <h1>Survey New</h1>;
+const Landing = () => <h1>Landing</h1>;
 
 class App extends Component {
   
   render() {
     return (
       <div>
-        <h1>Hi There</h1>
-      </div>
+        <BrowserRouter>
+          <div>          
+            <Header />
+            <Route path="/" exact component={Landing} />
+            <Route path="/survey" exact component={DashBoard}/>
+            <Route path="/survey/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>      
     );
   }
 }
